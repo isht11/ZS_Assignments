89596c0c30194fa44ecdd900cdd524b71222f9d2
Isht11 <ishtmeet.arora@zopsmart.com>
2022-02-17 10:44:22 +0530

    Assignment1 | Ishtmeet | Getting System Info Updated

--- src/main/java/com/zs/assignment1/systemInfo.java
+++ src/main/java/com/zs/assignment1/systemInfo.java
@@ -1,14 +1,13 @@
-package src.main.java.com.zs.assignment1;
-import java.io.*;
+package com.zs.assignment1;
+
 import java.io.File;
-import java.util.*;
 
 /**
  * @author Ishtmeet Singh Arora
  * This java file displays System Information.
  */
 
-public class SystemInfo {
+public class systemInfo {
 
     /**
      * Created a main function to display username, user home directory, Memory,  cores, name and version of OS.
@@ -16,16 +15,16 @@ public class SystemInfo {
      */
 
     public static void main(String[] args) {
-        String username = System.getProperty("user.name");
+        String userName = System.getProperty("user.name");
         String userHomeDir = System.getProperty("user.home");
-        Runtime Memory = Runtime.getRuntime();
+        Runtime memory = Runtime.getRuntime();
         String nameOfOs = System.getProperty("os.name");
         String versionOfOS = System.getProperty("sun.arch.data.model");
         long file = new File("/home").getUsableSpace();
         int cores = Runtime.getRuntime().availableProcessors();
-        System.out.println("username = " + username);
+        System.out.println("username = " + userName);
         System.out.println("The User Home Directory is "+userHomeDir);
-        System.out.println("Total memory is: " + Memory.totalMemory());
+        System.out.println("Total memory is: " + memory.totalMemory());
         System.out.println("Total no. of cores are: " + cores);
         System.out.println("Total Space is: " + file);
         System.out.println("Name of the operating system is: " + nameOfOs);
